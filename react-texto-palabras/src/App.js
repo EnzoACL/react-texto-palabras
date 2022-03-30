644eadb2e1f7b1f11e4708a5ff7762f5e061f21f
EnzoACL <afdm@postal6.academiapostal.es.local>
2022-03-30 13:31:37 +0200

    Componente Palabras modificado

--- react-texto-palabras/src/App.js
+++ react-texto-palabras/src/App.js
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { Component, useEffect, useState } from 'react';
 import './App.css';
 import ComponentLetras from './components/Letras/Letras'
 import ComponentPalabras from './components/Palabras/Palabras';
@@ -6,22 +6,16 @@ import ComponentPalabras from './components/Palabras/Palabras';
 function App() {
   const [text, setText] = useState("")
   function writingInTextBoxHandler(event) {
-    setText(event.target.value)
-    console.log({ text })
-    //Palabras
-    setSaveWords(text.split([" "]))
-    console.log(saveWords)
+    setText(event.target.value)    
   }
-  const [saveWords, setSaveWords] = useState([])
- 
-  
   
 return (
     <>
       <h1>Texto</h1>
       <textarea onChange={writingInTextBoxHandler} value={text}></textarea>
       <ComponentLetras letras={text}/>
-      <ComponentPalabras palabras={saveWords}/>
+      <ComponentPalabras palabras={text}/>
+      <input type="button" value="Console logs" ></input>
     </>
   );
 }
--- react-texto-palabras/src/components/Palabras/Palabras.jsx
+++ react-texto-palabras/src/components/Palabras/Palabras.jsx
@@ -1,8 +1,18 @@
 function ComponentPalabras(prop) {
+      //Palabras: elimino los espacios en blanco al final/principio del array
+    //con trim() para que no cuente como palabras hasta que escriba un espacio
+    //y un caracter. Luego convierto el nuevo strin en un array y lo asigno
+    //con setSaveWords.
+    const textTrim=prop.palabras.trim() 
+    const arraySplit = (textTrim.split([" "]))
+    const arrayFiltrado = arraySplit.filter(
+        string => string !=="" && string !== "/n"
+    );
+   
     return (
         <>
             <p>Contador de palabras</p>
-            <p>Numero de palabras: {prop.palabras.length}</p>
+            <p>Numero de palabras: {arrayFiltrado.length}</p>
         </>
     );
 }
